471cac614dc8ebd912d51f0be2822d12672d9be0
Chrilleweb <christian.munknissen@gmail.com>
2024-03-26 14:15:50 +0100

    Ready for deployment

--- routes/loginRegister.js
+++ routes/loginRegister.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const loginRegisterController = require("../controller/loginRegisterController");
 
-router.post("/signup", loginRegisterController.register_post);
+router.post("/register", loginRegisterController.register_post);
 router.post("/login", loginRegisterController.login_post);
 
 module.exports = router;
